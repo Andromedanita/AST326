d81862bba4d8d5a0fbdb91e17b315d77de63da85
Anita Bahmanyar <anita@user3-23-201.wireless.utoronto.ca>
2014-02-27 15:39:47 -0500

    works

--- Lab5/centroids.py
+++ Lab5/centroids.py
@@ -8,11 +8,9 @@ star=pf.getdata(stars)
 
 
 w=[72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89]  # suffixes for  flat files
-#t=[43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63] # suffixes for bias files
 t=[1599995,1599996,1599997,1599998,1599999,1600000,1600001,1600002,1600003,1600004,1600005,1600006,1600007,1600008,1600009,1600010,1600011,1600012,1600013,1600014,1600015,1600016,1600017,1600018,1600019,1600020]
-
-############## bias ##################
 '''
+############## bias ##################
 bias_array=np.zeros((2071,2148))
 
 #computing bias median
@@ -116,43 +114,49 @@ while m < len(max_x):
     centroid_y_list.append(centroids_y)
     #final_centroids=np.column_stack((centroid_x_list,centroid_y_list))
     #np.savetxt('centroids.txt',final_centroids)
-    
-    for l in range(0,2071):
-        for p in range(0,2148):
-            #distance_x=corrected[l][p]-centroids_x
-            #distance_y=corrected[l][p]-centroids_y
-            distance=np.sqrt(((corrected[l][p]-centroids_x)**2)+((corrected[l][p]-centroids_y)**2))
-            if distance<15:
-                flux_list.append(corrected[l][p])
-    
     m+=1
 '''
 
-flux_list=[]
-background_list=[]
-distances=[]
+########## calculation of flux of each star #########
+p_list=[]
+l_list=[]
 
+distances=[]
+temp_flux=np.zeros(35)
+temp_background=[]
+temp_flux=[]
 centroid_x_list=np.loadtxt('/Users/anita/Documents/University_Third_Year/AST326/Lab5/cent.txt',usecols=(0,))
 centroid_y_list=np.loadtxt('/Users/anita/Documents/University_Third_Year/AST326/Lab5/cent.txt',usecols=(1,))
+#predicted_radius=np.arange(0,15,1.756224351806862e-05)
+#predicted_radius=0
+i =0
+while i < 35:
+    r =centroid_x_list[i]
+    u =centroid_y_list[i]
+    box_x=np.arange(-26,26,1)  #box in x direction
+    box_y=np.arange(-26,26,1)  #box in y direction
+    #temp_flux=[]
+    #temp_flux=np.zeros(35)
+    total_flux=[]
+    flux_list=[]
+    background_list=[]
+    for l in (box_x+r):
+        for p in (box_y+u):
+            distance=np.sqrt(((l-r)**2)+((p-u)**2)) #distance between centroid and the points in the box
+            distances.append(distance)
+            print "distance is:", distance
+            if distance <15:
+                flux_list.append(corrected[l][p]) #appends the flux of the point if inside radius 15
+            elif distance<25 and distance>22:
+                background_list.append(corrected[l][p])  #appends the background if inside annulus(22<r<25)
+    flux_sum=np.sum(flux_list)
+    temp_flux.append(flux_sum)
+    #temp_flux[i] = total_flux
+    background_avg=np.mean(background_list)
+    temp_background.append(background_avg)
+    i+=1
 
-for r in centroid_x_list:
-    for u in centroid_y_list:
-        box_x=np.arange(-15,15,1)  #box in x direction
-        box_y=np.arange(-15,15,1)  #box in y direction
-        for l in (box_x+r):
-            for p in (box_y+u):
-                distance=np.sqrt(((l-r)**2)+((p-u)**2))
-                distances.append(distance)
-                print "distance is:", distance
-                if distance <15:
-                    flux_list.append(corrected[l][p])
-                    #plt.plot(l,p,'g.')
-                elif distance<25 and distance>22:
-                    background_list.append(corrected[l][p])
-                    #plt.plot(l,p,'m.')
-flux=np.sum(flux_list)
-print "flux is:", flux
-background=np.sum(background_list)
+
 
 #circle_x1=centroids_x+(10*(np.cos(theta)))
 #   circle_y1=centroids_y+(10*(np.sin(theta)))
@@ -165,34 +169,44 @@ background=np.sum(background_list)
 #   plt.plot(circle_x3,circle_y3,'k')
 # m+=1
 
+'''
+##### growth curve ######
 
+flux_list=[]
+distances=[]
+for l in range(0,2071):     #to only consider a box around the stars
+    for p in range(0,2148):
+        distance=np.sqrt(((l-410.54895642)**2)+((p-127.45104358)**2)) #distance between centroid and the points in the box
+        distances.append(distance)
+        print "distance is:", distance
+        for predicted_radius in range(0,15):
+            if distance <predicted_radius:
+                flux_list.append(corrected[l][p]) #appends the flux of the point if inside radius 15
+            plt.plot(predicted_radius,flux_list)
 
-############# signa; to noise ratio ###########
+'''
 
-
+#############  noise  ###########
+q=np.array(temp_background)
 r1=15
 r2=22
 r3=25
 readout_noise=6.0/1.9
 N1=(np.pi)*(r1**2)
 N23=(np.pi)*((r3**2)-(r2**2))
-B=background/(np.size(background_list))
-#Signal_noise=
+B=q/(np.size(temp_background))
+poisson_noise=np.std(temp_flux)
+total_noise=(poisson_noise)+(N1*(B+(readout_noise**2)))+((N1*(B+(readout_noise**2)))/N23)
+signal_to_noise=poisson_noise/(np.sqrt(total_noise))
 
 
-
+########### plots ################
 '''
-xx=[5,10,15,20,25]
-yy=[9567.1173956363182,37588.853678160653,86018.21664802068,152341.47112340882,237798.29647247944]
-plt.plot(xx,yy)
-'''
-
-########### plots ################hb u
 plt.imshow(corrected,origin= 'lower',interpolation='nearest')
 plt.plot(centroid_x_list,centroid_y_list,'y*')
 plt.xlim(0,2150)
 plt.ylim(0,2090)
 plt.title("Centroid of the star and the sky annulus")
 plt.colorbar() #showing the color bar
-
+'''
 plt.show()
